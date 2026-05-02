688750e5f870b12a67bba2c71740bfbf7d044299
kumagai <meteokuma@gmail.com>
2026-05-03 06:53:55 +0900

    updated lua file of ai

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,10 +5,5 @@ require("core.autocmds")
 -- AI tool
 
 require("plugins.lazy").setup({
-  { import = "plugins.files" },
-  { import = "plugins.lsp" },
-  { import = "plugins.git" },
-  { import = "plugins.ui" },
-  { import = "plugins.editor" },
   { import = "plugins.ai" },
 })
---
+++ nvim/lua/plugins/lazy.lua
@@ -0,0 +1,24 @@
+-- lua/plugins/lazy.lua
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local M = {}
+
+function M.setup(spec)
+  require("lazy").setup(spec)
+end
+
+return M
